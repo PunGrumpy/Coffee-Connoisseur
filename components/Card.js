7ef003617bf7dcb7510cc90356fff6991e822caa
PunGrumpy <noppakorn.pun@gmail.com>
2022-11-01 22:18:53 +0700

    fix(styling): card component

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,25 @@
 import styles from './Card.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import cls from 'classnames';
 
 const Card = props => {
     return (
-        <Link href={props.href}>
-            <h2>{props.name}</h2>
-            <Image src={props.imgUrl} width={260} height={160} alt={props.name} />
+        <Link className={styles.cardLink} href={props.href}>
+            <div className={cls('glass', styles.container)}>
+                <div className={styles.cardHeaderWrapper}>
+                    <h2 className={styles.cardHeader}>{props.name}</h2>
+                </div>
+                <div className={styles.cardImageWrapper}>
+                    <Image
+                        className={styles.cardImage}
+                        src={props.imgUrl}
+                        width={260}
+                        height={160}
+                        alt={props.name}
+                    />
+                </div>
+            </div>
         </Link>
     );
 };
