94340e829db07a48e31b5f18351df0bb549f9974
PunGrumpy <noppakorn.pun@gmail.com>
2022-11-03 22:30:01 +0700

    fix(*): coffee stores heading and get static props

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import Banner from '../components/Banner';
 import Card from '../components/Card';
 import styles from '../styles/Home.module.css';
-import coffeeStores from '../data/coffee-stores.json';
+import coffeeStoresData from '../data/coffee-stores.json';
 
 export async function getStaticProps(context) {
     return {
         props: {
-            coffeeStores,
+            coffeeStores: coffeeStoresData,
         },
     };
 }
@@ -32,17 +32,22 @@ export default function Home(props) {
                 <div className={styles.heroImage}>
                     <Image src="/static/hero-image.png" alt="HeroImg" width={700} height={400} />
                 </div>
-                <div className={styles.cardLayout}>
-                    {coffeeStores.map(coffeeStore => (
-                        <Card
-                            key={coffeeStore.id}
-                            name={coffeeStore.name}
-                            imgUrl={coffeeStore.imgUrl}
-                            href={`/coffee-cafe/${coffeeStore.id}`}
-                            className={styles.card}
-                        />
-                    ))}
-                </div>
+                {props.coffeeStores.length > 0 && (
+                    <>
+                        <h2 className={styles.heading2}>Coffee Stores</h2>
+                        <div className={styles.cardLayout}>
+                            {props.coffeeStores.map(coffeeStore => (
+                                <Card
+                                    key={coffeeStore.id}
+                                    name={coffeeStore.name}
+                                    imgUrl={coffeeStore.imgUrl}
+                                    href={`/coffee-cafe/${coffeeStore.id}`}
+                                    className={styles.card}
+                                />
+                            ))}
+                        </div>
+                    </>
+                )}
             </main>
         </div>
     );
