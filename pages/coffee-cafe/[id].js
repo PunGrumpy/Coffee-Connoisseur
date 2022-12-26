3a94842420113bc6d5ba79aefe07918dc96bf6ba
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-27 00:26:28 +0700

    refactor(page): change coffee store page

--- pages/coffee-cafe/[id].js
+++ pages/coffee-cafe/[id].js
@@ -1,14 +1,55 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import coffeeStoresData from '../../data/coffee-stores.json';
+import Head from 'next/head';
 
-const CoffeeCafe = () => {
+export function getStaticProps(staticProps) {
+    const params = staticProps.params;
+    console.log('params', params);
+
+    return {
+        props: {
+            coffeeStore: coffeeStoresData.find((coffeeStore) => {
+                return coffeeStore.id.toString() === params.id;
+            }),
+        },
+    };
+}
+
+export function getStaticPaths() {
+    return {
+        paths: coffeeStoresData.map((coffeeStore) => {
+            return {
+                params: {
+                    id: coffeeStore.id.toString(),
+                },
+            };
+        }),
+        fallback: true,
+    };
+}
+
+const CoffeeCafe = (props) => {
     const router = useRouter();
     console.log('Router', router);
 
+    const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
+
+    if (router.isFallback === true) return <div>Loading...</div>;
+
+    console.log('Props', props);
+
     return (
         <div>
-            Coffee Cafe Page {router.query.id}
+            <Head>
+                <title>{name}</title>
+            </Head>
             <Link href="/">Back to home</Link>
+            <p>{address}</p>
+            <p>{name}</p>
+            <p>{neighbourhood}</p>
+            <Image src={imgUrl} alt={name} width="500" height="500" />
         </div>
     );
 };
