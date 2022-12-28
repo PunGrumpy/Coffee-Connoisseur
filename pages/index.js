09fd8e88ea49e53bd5d2c3b33042535117e63284
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-28 22:23:25 +0700

    feat(*): add display coffee stores by location

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import coffeeStoresData from '../data/coffee-stores.json';
 import { fetchCoffeeStores } from '../lib/coffee-stores';
 
 import useTrackLocation from '../hooks/use-track-location';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export async function getStaticProps(context) {
     const coffeeStores = await fetchCoffeeStores();
@@ -26,7 +26,8 @@ export default function Home(props) {
     const { handlerTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
         useTrackLocation();
 
-    // console.log({ latLong, locationErrorMsg });
+    const [coffeeStores, setCoffeeStores] = useState('');
+    const [coffeeStoresError, setCoffeeStoresError] = useState(null);
 
     useEffect(() => {
         async function setCoffeeStoresByLocation() {
@@ -35,9 +36,11 @@ export default function Home(props) {
                     const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 6);
                     console.log({ fetchedCoffeeStores });
                     // set coffee stores
+                    setCoffeeStores(fetchedCoffeeStores);
                 } catch (error) {
                     // set error
                     console.log({ error });
+                    setCoffeeStoresError(error.message);
                 }
             }
         }
@@ -68,12 +71,38 @@ export default function Home(props) {
                         <span style={{ fontWeight: '600' }}>{locationErrorMsg}</span>
                     </p>
                 )}
+                {coffeeStoresError && (
+                    <p>
+                        Something went wrong:{' '}
+                        <span style={{ fontWeight: '600' }}>{coffeeStoresError}</span>
+                    </p>
+                )}
                 <div className={styles.heroImage}>
                     <Image src="/static/hero-image.png" alt="HeroImg" width={700} height={400} />
                 </div>
+                {coffeeStores.length > 0 && (
+                    <div className={styles.sectionWrapper}>
+                        <h2 className={styles.heading2}>Coffee stores near me</h2>
+                        <div className={styles.cardLayout}>
+                            {coffeeStores.map((coffeeStore) => (
+                                <Card
+                                    key={coffeeStore.id}
+                                    name={coffeeStore.name}
+                                    imgUrl={
+                                        coffeeStore.imgUrl ||
+                                        'https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80'
+                                    }
+                                    href={`/coffee-store/${coffeeStore.id}`}
+                                    className={styles.card}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
+
                 {props.coffeeStores.length > 0 && (
                     <div className={styles.sectionWrapper}>
-                        <h2 className={styles.heading2}>Coffee Stores</h2>
+                        <h2 className={styles.heading2}>Coffee stores</h2>
                         <div className={styles.cardLayout}>
                             {props.coffeeStores.map((coffeeStore) => (
                                 <Card
