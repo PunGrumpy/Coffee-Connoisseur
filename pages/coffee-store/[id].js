62c7a21a6f4c11cf89e084c1b5268acabac5b013
PunGrumpy <noppakorn.pun@gmail.com>
2022-12-28 20:35:09 +0700

    feat(*): add unplash to app

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -8,7 +8,6 @@ import coffeeStoresData from '../../data/coffee-stores.json';
 import { fetchCoffeeStores } from '../../lib/coffee-stores';
 
 import styles from '../../styles/coffee-stores.module.css';
-import react from 'react';
 
 export async function getStaticProps(staticProps) {
     const params = staticProps.params;
@@ -17,7 +16,7 @@ export async function getStaticProps(staticProps) {
     return {
         props: {
             coffeeStore: coffeeStores.find((coffeeStore) => {
-                return coffeeStore.fsq_id.toString() === params.id;
+                return coffeeStore.id.toString() === params.id;
             }),
         },
     };
@@ -29,7 +28,7 @@ export async function getStaticPaths() {
     const paths = coffeeStores.map((coffeeStore) => {
         return {
             params: {
-                id: coffeeStore.fsq_id.toString(),
+                id: coffeeStore.id.toString(),
             },
         };
     });
@@ -44,7 +43,7 @@ const CoffeeCafe = (props) => {
 
     if (router.isFallback === true) return <div>Loading...</div>;
 
-    const { name, location, related_places, imgUrl } = props.coffeeStore;
+    const { name, address, neighborhood, imgUrl } = props.coffeeStore;
 
     const handlerUpVoteButton = () => {
         console.log('handler upvote');
@@ -58,7 +57,7 @@ const CoffeeCafe = (props) => {
             <div className={styles.container}>
                 <div className={styles.col1}>
                     <div className={styles.backToHomeLink}>
-                        <Link href="/">Back to home</Link>
+                        <Link href="/">← Back to home</Link>
                     </div>
                     <div className={styles.nameWrapper}>
                         <h1 className={styles.name}>{name}</h1>
@@ -75,20 +74,28 @@ const CoffeeCafe = (props) => {
                     />
                 </div>
                 <div className={cls('glass', styles.col2)}>
-                    <div className={styles.iconWrapper}>
-                        <Image src="/static/icons/places.svg" width="24" height="24" alt="places" />
-                        <p className={styles.text}>
-                            {location.address ? location.address : location.formatted_address}
-                        </p>
-                    </div>
-                    <div className={styles.iconWrapper}>
-                        <Image src="/static/icons/nearMe.svg" width="24" height="24" alt="nearMe" />
-                        <p className={styles.text}>
-                            {location.neighborhood
-                                ? location?.neighborhood
-                                : related_places.parent?.name}
-                        </p>
-                    </div>
+                    {address && (
+                        <div className={styles.iconWrapper}>
+                            <Image
+                                src="/static/icons/places.svg"
+                                width="24"
+                                height="24"
+                                alt="places"
+                            />
+                            <p className={styles.text}>{address}</p>
+                        </div>
+                    )}
+                    {neighborhood && (
+                        <div className={styles.iconWrapper}>
+                            <Image
+                                src="/static/icons/nearMe.svg"
+                                width="24"
+                                height="24"
+                                alt="nearMe"
+                            />
+                            <p className={styles.text}>{neighborhood}</p>
+                        </div>
+                    )}
                     <div className={styles.iconWrapper}>
                         <Image src="/static/icons/star.svg" width="24" height="24" alt="star" />
                         <p className={styles.text}>1</p>
